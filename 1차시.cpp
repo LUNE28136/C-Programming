397252b82af0938882501c78f906701e8a095fe9
박서현 <126850075+LUNE28136@users.noreply.github.com>
2023-09-08 12:17:53 +0900

    Add files via upload

---
+++ 1차시.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+/*
+int main(void)
+{
+	printf("Hello everyone! \n");
+	printf("%d \n",1234);
+	printf("%d %d \n",10,20);
+	return 0;
+}
+*/
+
+/*
+//2.1.
+int main(void)
+{
+	printf("홍길동\n홍 길 동\n홍  길  동");
+	return 0;
+}
+*/
+
+/*
+//2.2.
+int main(void)
+{
+	printf("이름: 박서현 \n");
+	printf("주소: 경기도 고양시 일산서구 현중로 64 \n");
+	printf("전화번호: 010-%d-%d \n", 8246, 1883);
+	return 0;
+}
+*/
+
+//HW
+int main(void)
+{
+	printf("C프로그래밍 1주차 과제 \n");
+	printf("이름: 박서현 \n");
+	printf("학번: %d%d \n", 2023, 10924);
+	return 0;
+}
